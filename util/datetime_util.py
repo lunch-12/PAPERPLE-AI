fe03ffa809b417e74db63926c317499e7f8008e5
Hansol Ju <hansol1501@gmail.com>
2024-08-28 21:19:11 +0900

    feat: 야후 크롤링 데이터를 RDS에 저장

--- util/datetime_util.py
+++ util/datetime_util.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 
 def convert_NAVER_date_to_datetime(date_str: str) -> datetime:
@@ -11,3 +11,19 @@ def convert_NAVER_date_to_datetime(date_str: str) -> datetime:
     date_obj = datetime.strptime(date_str, date_format)
 
     return date_obj.replace(tzinfo=timezone.utc)
+
+
+def convert_Yahoo_date_to_datetime(date_str: str) -> datetime:
+    # print("CONVERT YAHOO DATA BEFORE:", date_str)
+    base_str = date_str[:-6].strip()  # 'GMT+9' 제거
+    # print("SECOND BASE STR:", base_str)
+    parsed_date = datetime.strptime(base_str, "%a, %b %d, %Y, %I:%M %p")
+    # print("PARED DATE:", parsed_date)
+
+    offset_hours = 9
+    offset = timezone(timedelta(hours=offset_hours))
+    # print("OFFSET:", offset)
+    parsed_date = parsed_date.replace(tzinfo=offset)
+    # print("FINAL PARSED DATE:", parsed_date)
+
+    return parsed_date
