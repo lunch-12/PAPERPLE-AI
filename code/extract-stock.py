388b464c78a9d1c999419c8e5aa9f058214ebff5
JiwonHwang84 <ghkdwldnjs112@gmail.com>
2024-08-30 13:54:21 +0900

    feat: 종목명 처리 작업 추가

--- code/extract-stock.py
+++ code/extract-stock.py
@@ -15,13 +15,14 @@ def setup_openai_client():
 def read_excel_file(file_path):
     return pd.read_excel(file_path)
 
+
 # 뉴스 요약 및 관련 종목 추출
 def get_related_stock(client, content):
     cleaned_content = content.replace('\n', ' ').replace('\r', '').strip()[:800]
     response = client.chat.completions.create(
         model="gemma2",  # llama3.1
         messages=[
-            {"role": "system", "content": "다음 뉴스와 가장 관련있는 주식종목이름 1개만을 반환해라. 주식종목명만 반환. 만약 관련 종목이 없다면 ''을 반환하라"},
+            {"role": "system", "content": "다음 뉴스와 가장 관련있는 주식종목이름 1개만을 반환해라. 주식종목명만 반환. 만약 관련 종목이 없다면 'N/A'만을 반환하라"},
             {"role": "user", "content": cleaned_content}
         ]
     )
@@ -29,7 +30,7 @@ def get_related_stock(client, content):
 
 # 종목 코드 찾기
 def get_stock_code(stock, code_df):
-    if stock != '':
+    if stock != 'N/A':
         # 종목 이름을 소문자 또는 대문자로 비교하여 일치하는 행을 찾음
         matching_rows = code_df.loc[
             (code_df['name'].str.lower() == stock.lower()) | 
@@ -56,9 +57,21 @@ def fetch_stock_info(stock_code):
             print(f"Error fetching data for stock code {stock_code}: {e}")
     return 'N/A', 'N/A', 'N/A'
 
+# 정보 저장
+def fetch_and_append_stock_info(stock_code, row, results):
+    stock_name, current_price, last_price_change = fetch_stock_info(stock_code)
+    results.append({
+        '뉴스 제목': row['title'],
+        '관련 종목명': stock_name,
+        '종목 코드': stock_code,
+        '현재 가격': current_price,
+        '전일대비 등락가격': last_price_change
+    })
+    
+    
 # 종목명 처리 및 정보 가져오기
 def process_stock_info(stock, code_df, results, row):
-    if stock:
+    if stock != 'N/A':
         # 먼저 원래 이름으로 종목 코드 찾기 시도
         stock_code = get_stock_code(stock, code_df)
 
@@ -66,54 +79,52 @@ def process_stock_info(stock, code_df, results, row):
         if not stock_code:
             # 1. '035420'와 같이 종목명이 종목코드로 반환된 경우
             if re.match(r'^\d{6}$', stock):
-                stock_name, current_price, last_price_change = fetch_stock_info(stock)
-                results.append({
-                    '뉴스 제목': row['title'],
-                    '관련 종목명': stock_name,
-                    '종목 코드': stock,
-                    '현재 가격': current_price,
-                    '전일대비 등락가격': last_price_change
-                })
+                fetch_and_append_stock_info(stock, row, results)
                 return
 
             # 2. '네이버(035420)'와 같이 종목명에 괄호로 종목 코드가 포함된 경우
             match = re.search(r'\((\d{6})\)', stock)
             if match:
                 stock_code = match.group(1)
-                stock_name, current_price, last_price_change = fetch_stock_info(stock_code)
-                results.append({
-                    '뉴스 제목': row['title'],
-                    '관련 종목명': stock_name,
-                    '종목 코드': stock_code,
-                    '현재 가격': current_price,
-                    '전일대비 등락가격': last_price_change
-                })
+                fetch_and_append_stock_info(stock_code, row, results)
                 return
 
-            # 3. '네이버(주)'와 같이 괄호 포함 텍스트를 제거한 후 다시 시도
+            # 3. 'SS0IAQ (삼성전자)'와 같이 괄호로 종목명이 포함된 경우
+            match = re.search(r'\((.*?)\)', stock)
+            if match:
+                stock_name = match.group(1)  # 괄호 안의 종목명 추출
+                stock_code = get_stock_code(stock_name, code_df)
+                if stock_code:
+                    fetch_and_append_stock_info(stock_code, row, results)
+                    return
+            
+            # 4. '네이버(주)'와 같이 괄호 포함 텍스트를 제거한 후 다시 시도
             stock_name_cleaned = re.sub(r'\(.*?\)', '', stock).strip()
             stock_code = get_stock_code(stock_name_cleaned, code_df)
             if stock_code:
-                stock_name, current_price, last_price_change = fetch_stock_info(stock_code)
-                results.append({
-                    '뉴스 제목': row['title'],
-                    '관련 종목명': stock_name,
-                    '종목 코드': stock_code,
-                    '현재 가격': current_price,
-                    '전일대비 등락가격': last_price_change
-                })
+                fetch_and_append_stock_info(stock_code, row, results)
                 return
+
+            # 5. 영어 알파벳이 포함된 경우 한글로 변환 후 다시 시도
+            stock_translated = simple_filter(stock)
+            if stock_translated != stock:  # 변환된 텍스트가 있을 경우
+                stock_code = get_stock_code(stock_translated, code_df)
+                if stock_code:
+                    fetch_and_append_stock_info(stock_code, row, results)
+                    return
+                
+            # 6. 영어 고유명을 한글로 변환(MEDITEX->메디톡스)
+            stock_translated_2 = convert_english_name_to_korean(stock)
+            if stock_translated_2 != stock:  # 변환된 텍스트가 있을 경우
+                stock_code = get_stock_code(stock_translated_2, code_df)
+                if stock_code:
+                    fetch_and_append_stock_info(stock_code, row, results)
+                    return
+            
 
         # 종목 코드를 찾았을 경우 정보 가져오기
         if stock_code:
-            stock_name, current_price, last_price_change = fetch_stock_info(stock_code)
-            results.append({
-                '뉴스 제목': row['title'],
-                '관련 종목명': stock_name,
-                '종목 코드': stock_code,
-                '현재 가격': current_price,
-                '전일대비 등락가격': last_price_change
-            })
+            fetch_and_append_stock_info(stock_code, row, results)
         else:
             results.append({
                 '뉴스 제목': row['title'],
@@ -139,13 +150,61 @@ def save_results_to_excel(results, output_file):
     results_df.to_excel(output_file, index=False)
     print(f"결과가 '{output_file}' 파일로 저장되었습니다.")
 
+# 'NC소프트' >> '엔씨소프트'
+def simple_filter(input_text):
+    ENGS = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k',
+            'K', 'l', 'L',
+            'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w',
+            'W', 'x', 'X', 'y', 'Y', 'z', 'Z']
+
+    KORS = ['에이', '에이', '비', '비', '씨', '씨', '디', '디', '이', '이', '에프', '에프', '쥐', '쥐', '에이치', '에이치', '아이', '아이', '제이',
+            '제이',
+            '케이', '케이', '엘', '엘', '엠', '엠', '엔', '엔', '오', '오', '피', '피', '큐', '큐', '알', '알', '에스', '에스', '티', '티', '유',
+            '유', '브이', '브이',
+            '더블유', '더블유', '엑스', '엑스', '와이', '와이', '지', '지']
+
+    trans = dict(zip(ENGS, KORS)) # 영어와 한글을 대칭하여 딕셔너리화
+    # 변환된 결과를 저장할 리스트
+    result_trans = []
+    
+    # input_text의 각 문자에 대해
+    for char in input_text:
+        if char in trans:  # 문자가 영어 알파벳인 경우
+            result_trans.append(trans[char])  # 대응되는 한글 발음 추가
+        else:
+            result_trans.append(char)  # 영어 알파벳이 아닌 경우 원래 문자 그대로 추가
+    
+    # 결과 문자열로 합치기
+    return ''.join(result_trans)
+
+
+# 영어 고유명을 한글로 변환
+def convert_english_name_to_korean(name):
+    # 미리 정의된 영어 고유명 -> 한글 변환 매핑
+    name = name.replace(" ", "").upper()
+    name_mapping = {
+        "NCSOFT": "엔씨소프트",
+        "SAMSUNG": "삼성",
+        "MEDITEX": "메디톡스",
+        "LG": "엘지",
+        "NETMARBLE": "넷마블",
+        # 필요에 따라 더 추가
+    }
+    
+    # 매핑된 이름이 있으면 반환, 없으면 음역 적용
+    if name in name_mapping:
+        return name_mapping[name]
+    else:
+        return simple_filter(name)
+    
+    
 # 메인 함수
 def main():
     client = setup_openai_client()
     
     # 뉴스 기사 데이터 읽기
     df = read_excel_file('./data/article_df_240812.xlsx')
-    new_df = df.tail(20)
+    new_df = df.tail(100)
 
     # 한국거래소 종목 코드 데이터 읽기
     code_df = read_excel_file('data/국내상장법인.xlsx')
@@ -160,7 +219,7 @@ def main():
         process_stock_info(stock, code_df, results, row)
 
     # 결과를 새로운 엑셀 파일로 저장
-    save_results_to_excel(results, 'data/관련주식정보.xlsx')
+    save_results_to_excel(results, 'data/관련주식정보_top100.xlsx')
 
 if __name__ == "__main__":
     main()
