bf59acc68e460f54907282c550cfc10942709ccd
Hansol Ju <hansol1501@gmail.com>
2024-09-03 02:44:57 +0900

    feat: 지원하지 않는 플랫폼 예외 추가

--- ai_exception.py
+++ ai_exception.py
@@ -4,10 +4,20 @@ class UserNotFoundError(Exception):
 
 
 class InvalidURLError(Exception):
-    def __init__(self, message="The provided URL is invalid"):
+    def __init__(self, message="The URL Type is not matched"):
+        super().__init__(message)
+
+
+class URLNotFoundError(Exception):
+    def __init__(self, message="The provided URL was not found"):
         super().__init__(message)
 
 
 class URLNotCrawlableError(Exception):
     def __init__(self, message="The page cannot be crawled"):
+        super().__init__(message)
+
+
+class NotSupportedException(Exception):
+    def __init__(self, message="This platform does not support the service."):
         super().__init__(message)
