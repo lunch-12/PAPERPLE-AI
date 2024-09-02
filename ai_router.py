0f3dcd93ea442c5a40171f3d3b764e31ff685240
Hansol Ju <hansol1501@gmail.com>
2024-09-03 02:59:50 +0900

    feat: 페이지리스트 라우터 주석처리, 예외 상태코드추가(419), 메시지 수정

--- ai_router.py
+++ ai_router.py
@@ -17,29 +17,33 @@ ai_router = APIRouter()
     status_code=status.HTTP_201_CREATED,
     responses={
         201: {"description": "Successful Response and Create Newspaper"},
-        403: {"description": "Forbidden, URL Is Not Crawlable"},
-        404: {"description": "Not Found, URL Is Invalid"},
+        403: {"description": "Forbidden, The page cannot be crawled"},
+        404: {"description": "Not Found, The URL Type is not matched"},
+        419: {"description": "Not Support, This platform does not support the service"},
     },
 )
 def post_newspaper(URL: str) -> ai_model.APIMODEL.NewsPaper:
+    print("[START]post_newspaper, URL:", URL)
     try:
         return ai_service.crawl_and_write_newspaper(URL)
     except ai_exception.URLNotCrawlableError as e:
         raise HTTPException(status_code=403, detail={"message": e.args[0]})
     except ai_exception.InvalidURLError as e:
         raise HTTPException(status_code=404, detail={"message": e.args[0]})
+    except ai_exception.NotSupportedException as e:
+        raise HTTPException(status_code=419, detail={"message": e.args[0]})
     except Exception as e:
-        raise HTTPException(status_code=500, detail={"message": e.args[0]})
+        raise HTTPException(status_code=500, detail={"message": e})
 
 
-@ai_router.get(
-    "/newspapaers/{USER_ID}",
-    responses={404: {"description": "Not Found, Check User Id"}},
-)
-def get_newspapers(user_id: int) -> ai_model.APIMODEL.Newspapers:
-    try:
-        return ai_service.get_newspapers_for_user(user_id)
-    except ai_exception.UserNotFoundError as e:
-        raise HTTPException(status_code=404, detail={"message": e.args[0]})
-    except Exception as e:
-        raise HTTPException(status_code=500, detail={"message": e.args[0]})
+# @ai_router.get(
+#     "/newspapaers/{USER_ID}",
+#     responses={404: {"description": "Not Found, Check User Id"}},
+# )
+# def get_newspapers(user_id: int) -> ai_model.APIMODEL.Newspapers:
+#     try:
+#         return ai_service.get_newspapers_for_user(user_id)
+#     except ai_exception.UserNotFoundError as e:
+#         raise HTTPException(status_code=404, detail={"message": e.args[0]})
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail={"message": e.args[0]})
