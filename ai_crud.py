c80013eeaef9b556f111bd314151de877674a3d3
Hansol Ju <hansol1501@gmail.com>
2024-09-03 02:52:04 +0900

    feat: link_hash를 인자로 DB에서 newspaper를 read하는 메서드 추가

--- ai_crud.py
+++ ai_crud.py
@@ -1,5 +1,5 @@
 import yaml
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import create_engine, SQLModel, Session, select
 from sqlalchemy.dialects.mysql import insert
 from ai_model import SQLMODEL
 
@@ -38,3 +38,30 @@ def upsert_newspapers(newspapers: list[SQLMODEL.NewsPaper]):
         except Exception as e:
             session.rollback()
             raise e
+
+
+def read_newspaper(link_hash: str) -> SQLMODEL.NewsPaper:
+    print("[START]read_newspaper, link_hash:", link_hash)
+    with Session(engine, expire_on_commit=False) as session:
+        try:
+            statement = select(SQLMODEL.NewsPaper).where(
+                SQLMODEL.NewsPaper.link_hash == link_hash
+            )
+            newspaper = session.exec(statement).one()
+            return newspaper
+        except Exception as e:
+            print("[EXCEPTION]", e)
+            raise ValueError
+
+
+# def test():
+#     try:
+#         newspaper = read_newspaper(
+#             link_hash="a2de8839d7f32bb07bd9505612796d7abda66a8bc2a32e0a7ac046f20f279fb2"
+#         )
+#         print(newspaper.__dir__)
+#     except Exception:
+#         print("ERROR")
+
+
+# test()
